e962720aae2ba3539eadc6b20bc9cfc080ed2196
fc55141 <fc55141@alunos.fc.ul.pt>
2023-02-06 00:04:04 +0000

    commit n1 tomcat

---
+++ HelloWorldWeb/src/main/java/fcul/css/HelloWorldWeb/App.java
@@ -0,0 +1,13 @@
+package fcul.css.HelloWorldWeb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
